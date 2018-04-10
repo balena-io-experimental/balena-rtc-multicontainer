b0d1b4bbd47ad75e899792c558c17a322a6d18a2
Gergely Imreh <imrehg@gmail.com>
2018-04-10 14:09:29 +0100

    updated

--- rtc/start.sh
+++ rtc/start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Parse RTC date into unixtime
 rtcdate=$(hwclock -l)
-# required to format the date string on busybox
-rtcdate="${rtcdate%%0.000000 seconds}"
-unix_rtcdate=$(date -d "${rtcdate}" "+%s")
+# required to format the date string on busybox date, not requir
+unix_rtcdate=$(date -D "%c" -d "${rtcdate}" "+%s")
+# With coreutils date the conversion should be
+# unix_rtcdate=$(date -d "${rtcdate}" "+%s")
+
+# Get current system unixtime
 unix_systemdate=$(date "+%s")
+
+# Only restore time if it's ahead of system time
 if [ "${unix_rtcdate}" -gt "${unix_systemdate}" ]; then
    echo "Restoring system time from RTC"
    hwclock --hctosys
@@ -12,4 +18,5 @@ else
    echo "System time is ahead of RTC, not restoring"
 fi
 
+## Do the main payload
 while : ; do date; sleep "${INTERVAL:-15}"; done
