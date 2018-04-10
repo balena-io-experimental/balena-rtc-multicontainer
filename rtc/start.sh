ec589c3117e2b833acfae2a9c2561daca92c7985
Gergely Imreh <imrehg@gmail.com>
2018-04-10 13:48:39 +0100

    updated

--- rtc/start.sh
+++ rtc/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 rtcdate=$(hwclock -l)
-unix_rtcdate=$(date -d "${rtcdata}" "+%s")
+# required to format the date string on busybox
+rtcdate="${rtcdate%%0.000000 seconds}"
+unix_rtcdate=$(date -d "${rtcdate}"  seconds}" "+%s")
 unix_systemdate=$(date "+%s")
-if [ ${unix_rtcdate} -ge ${unix_systemdata} ]; then
+if [ ${unix_rtcdate} -ge ${unix_systemdate} ]; then
    echo "Restoring system time from RTC"
    hwclock --hctosys
 else
